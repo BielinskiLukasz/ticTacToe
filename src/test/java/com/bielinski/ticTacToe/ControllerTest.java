819f97b3f9ba3cbce6170ac590d906830123450e
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2018-04-29 13:43:49 +0200

    junit, assertj and mockito added

---
+++ src/test/java/com/bielinski/ticTacToe/ControllerTest.java
@@ -0,0 +1,33 @@
+package com.bielinski.ticTacToe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    //shouldDoSthWhenSth
+    //given
+    //when
+    //then
+
+    @Test
+    public void move() {
+    }
+
+    @Test
+    public void afterMove() {
+    }
+
+    @Test
+    public void restartGame() {
+    }
+
+    @Test
+    public void startGameWithAI() {
+    }
+
+    @Test
+    public void moveAI() {
+    }
+}
