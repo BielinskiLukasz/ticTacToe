2deac3113c05faa9470cec3a9f2f639091da1b3f
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2018-09-16 22:54:02 +0200

    Add AIminimax tests

---
+++ src/test/java/com/bielinski/ticTacToe/AIminimaxTest.java
@@ -0,0 +1,57 @@
+package com.bielinski.ticTacToe;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AIminimaxTest {
+
+    @Test
+    public void shouldReturnMiddleElementWhenAllFieldsAreOccupied() {
+        //given
+        int minValue = Integer.MIN_VALUE;
+        int[] fieldPointValue = new int[9];
+        for (int i = 0; i < fieldPointValue.length; i++) {
+            fieldPointValue[i] = minValue;
+        }
+        //when
+        int bestFieldIndex = AIminimax.findBestFieldIndex(fieldPointValue);
+        //then
+        assertThat(bestFieldIndex).isEqualTo(fieldPointValue.length / 2);
+    }
+
+    @Test
+    public void shouldReturn1WhenFieldWithIndex1HaveMaxValue() {
+        //given
+        int[] fieldPointValue = {
+                256, 512, 256,
+                128, 64, 32,
+                4, 8, 16
+        };
+        //when
+        int bestFieldIndex = AIminimax.findBestFieldIndex(fieldPointValue);
+        //then
+        assertThat(bestFieldIndex).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldReturnPlayerXForPlayerO() {
+        //given
+        Player actualPlayer = Player.PLAYER_O;
+        //when
+        actualPlayer = AIminimax.switchPlayer(actualPlayer);
+        //then
+        assertThat(actualPlayer).isEqualTo(Player.PLAYER_X);
+    }
+
+    @Test
+    public void shouldReturnPlayerOForPlayerO() {
+        //given
+        Player actualPlayer = Player.PLAYER_X;
+        //when
+        actualPlayer = AIminimax.switchPlayer(actualPlayer);
+        //then
+        assertThat(actualPlayer).isEqualTo(Player.PLAYER_O);
+    }
+
+}
