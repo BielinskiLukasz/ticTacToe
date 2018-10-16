1429347ea9dd7cccc4516010717b80f1adc9fff1
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2018-10-17 00:28:04 +0200

    Add players field code constant values

--- src/main/java/com/bielinski/ticTacToe/Player.java
+++ src/main/java/com/bielinski/ticTacToe/Player.java
@@ -4,11 +4,19 @@ public enum Player {
     X,
     O;
 
+    static final int PLAYER_X_CODE = 1;
+    static final int PLAYER_Y_CODE = -1;
+    static final int NONE_PLAYER_CODE = 0;
+
     public Player nextPlayer() {
         return this == X ? O : X;
     }
 
     public static Player randomChooseFirstPlayer() {
         return (Math.random() < 0.5) ? Player.X : Player.O;
+    }
+
+    public int fieldCode() {
+        return this == X ? PLAYER_X_CODE : PLAYER_Y_CODE;
     }
 }
