46edd74787fda4fefc330af6864d8da704b32b34
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2018-10-16 23:54:08 +0200

    Add takeWinningCombination method and refactor status checking - now it's based on static int table (one table to all methods)

--- src/main/java/com/bielinski/ticTacToe/GameStatusChecker.java
+++ src/main/java/com/bielinski/ticTacToe/GameStatusChecker.java
@@ -2,15 +2,25 @@ package com.bielinski.ticTacToe;
 
 class GameStatusChecker {
 
+    private static int[][] winningCombinations = {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8},
+            {0, 3, 6},
+            {1, 4, 7},
+            {2, 5, 8},
+            {0, 4, 8},
+            {2, 4, 6}
+    };
+
     static boolean isWinner(int[] checkBoard) {
-        return isItAWinningCombination(checkBoard[0], checkBoard[1], checkBoard[2]) ||
-                isItAWinningCombination(checkBoard[3], checkBoard[4], checkBoard[5]) ||
-                isItAWinningCombination(checkBoard[6], checkBoard[7], checkBoard[8]) ||
-                isItAWinningCombination(checkBoard[0], checkBoard[3], checkBoard[6]) ||
-                isItAWinningCombination(checkBoard[1], checkBoard[4], checkBoard[7]) ||
-                isItAWinningCombination(checkBoard[2], checkBoard[5], checkBoard[8]) ||
-                isItAWinningCombination(checkBoard[0], checkBoard[4], checkBoard[8]) ||
-                isItAWinningCombination(checkBoard[2], checkBoard[4], checkBoard[6]);
+        boolean isWinner = false;
+        for (int[] winningCombination : winningCombinations) {
+            isWinner = isWinner || isItAWinningCombination(checkBoard[winningCombination[0]],
+                    checkBoard[winningCombination[1]],
+                    checkBoard[winningCombination[2]]);
+        }
+        return isWinner;
     }
 
     private static boolean isItAWinningCombination(int field1, int field2, int field3) {
@@ -20,19 +30,29 @@ class GameStatusChecker {
     }
 
     static boolean isDraw(int[] checkBoard) {
-        return isWinningCombinationUnavailable(checkBoard[0], checkBoard[1], checkBoard[2]) &&
-                isWinningCombinationUnavailable(checkBoard[3], checkBoard[4], checkBoard[5]) &&
-                isWinningCombinationUnavailable(checkBoard[6], checkBoard[7], checkBoard[8]) &&
-                isWinningCombinationUnavailable(checkBoard[0], checkBoard[3], checkBoard[6]) &&
-                isWinningCombinationUnavailable(checkBoard[1], checkBoard[4], checkBoard[7]) &&
-                isWinningCombinationUnavailable(checkBoard[2], checkBoard[5], checkBoard[8]) &&
-                isWinningCombinationUnavailable(checkBoard[0], checkBoard[4], checkBoard[8]) &&
-                isWinningCombinationUnavailable(checkBoard[2], checkBoard[4], checkBoard[6]);
+        boolean isDraw = true;
+        for (int[] winningCombination : winningCombinations) {
+            isDraw = isDraw && isWinningCombinationUnavailable(checkBoard[winningCombination[0]],
+                    checkBoard[winningCombination[1]],
+                    checkBoard[winningCombination[2]]);
+        }
+        return isDraw;
     }
 
     private static boolean isWinningCombinationUnavailable(int field1, int field2, int field3) {
         return (!(field1 == 0) || !(field2 == 0) || !(field3 == 0)) &&
                 ((field1 == 1) || (field2 == 1) || (field3 == 1)) &&
                 ((field1 == -1) || (field2 == -1) || (field3 == -1));
+    }
+
+    static int[] takeWinningCombination(int[] checkBoard) {
+        int[] currentWiningCombination = null;
+        for (int[] winningCombination : winningCombinations) {
+            if (isItAWinningCombination(checkBoard[winningCombination[0]],
+                    checkBoard[winningCombination[1]],
+                    checkBoard[winningCombination[2]]))
+                currentWiningCombination = winningCombination;
+        }
+        return currentWiningCombination;
     }
 }
